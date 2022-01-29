678574cc4824c0a0cf3e4f2d3319738e775959e8
Grzegorz Szpura <g.szpura@gmail.com>
2022-01-29 17:27:33 +0100

    Adds convergence cone calc

--- nozzle.py
+++ nozzle.py
@@ -1,43 +1,69 @@
 # TODO: visualisation
 import math
 
-print("-----------------------DIVERGENCE----------------------------")
+# CONSTS
 # diamater of throat
 dt = 5
+rt = dt / 2.0
+# diamater of combustion chamber
+dc = 18
+rc = dc / 2.0
 # expansion ratio
 exp = 4.84
 # div angle
 div = 30
-div_half = div / 2
-div_rad = div_half / 180.0 * math.pi
-print("Init conditions  ", "throat: ", dt, "expansion: ", exp, "divergence:", div)
+conv_deg = 60
+print("Init conditions  ", "throat: ", dt, "expansion: ", exp, "divergence angle:", div, "conv angle:", conv_deg)
 
-at = math.pi*(dt/2)**2
+
+# COMMON CALC
+# area of throat
+at = math.pi*(rt)**2
+
+
+print("-----------------------DIVERGENCE---------------------------------")
+# calc for exit values
 ae = at*exp
 re = math.sqrt(ae/math.pi)
 print("Exit radius:", re)
 print("Exit diameter:", re*2)
-print("Divergence half radians:", div_rad, "Tanges:", math.tan(div_rad))
-# calculations on divergance trapezoid
-div_len = (re - (dt/2)) / math.tan(div_rad)
+
+# calc for div angles
+div_half = div / 2
+div_rad = div_half / 180.0 * math.pi
+div_tan = math.tan(div_rad)
+# calculations for divergance trapezoid
+div_len = (re - rt) / div_tan
 print("Divergence length:", div_len)
 
 
+print("-----------------------DIVERGENCE FORM----------------------------")
+div_cone_missing_h =  rt*div_len / (re - rt)
+div_cone_h = div_cone_missing_h + div_len
+print("DIV CONE H:", div_cone_h)
+print("DIV CONE D:", re*2)
 
-# -------------------------------- convergence ------------------
-print("-----------------------CONV----------------------------")
-conv_deg = 30
-conv_rad = conv_deg/180.0 * math.pi
+
+print("-----------------------CONVERGENCE--------------------------------")
+# calc for conv angles
+conv_deg_half = conv_deg / 2
+conv_rad = conv_deg_half/180.0 * math.pi
 tan_conv = math.tan(conv_rad)
 
-chamber_d = 18
-chamber_r = chamber_d/2
-chamber_r_trapezoid = 9 - dt/2
-chamber_len = chamber_r_trapezoid / tan_conv
-print("Convergence length:", chamber_len)
+# calc for convergence trapezoid
+chamber_r_trapezoid = rc - rt
+conv_len = chamber_r_trapezoid / tan_conv
+print("Convergence length:", conv_len)
 
 
-# ----- expansion ration calc -----
+print("-----------------------CONVERGENCE FORM----------------------------")
+conv_cone_missing_h = rt*conv_len/(rc - rt)
+conv_cone_h = conv_cone_missing_h + conv_len
+print("CONV CONE H:", conv_cone_h)
+print("CONV CONE D:", dc)
+
+
+# ----- expansion ratio calc -----
 # http://www.nakka-rocketry.net/articles/noz_example3.pdf
 # KNSU: https://www.nakka-rocketry.net/succhem.html
 print("-----------------------TODO EXPANSION RATIO--------------------------")
